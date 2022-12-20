2746e713d03ecf3a0524e8e4c601f4333fccb13d
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-20 17:46:00 -0600

    Basic landing page layout done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
@@ -35,7 +34,7 @@ function App() {
         <Route path='/login' exact>
           <LoginForm />
         </Route>
-        <Route path='/sign-up' exact>
+        <Route path='/join' exact>
           <SignUpForm />
         </Route>
         <ProtectedRoute path='/users' exact >
--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -1,39 +1,60 @@
-import styles from "../stylesheets/LandingPage.module.css";
 import NavBar from "./NavBar";
-
+import { useHistory } from "react-router-dom";
+import banner from "../assets/landing-banner.png";
+import heroBiking from "../assets/hero-biking.webp";
+import heroRunning from "../assets/hero-running.webp";
+import phone1 from "../assets/landing-page-phone1.webp";
+import runningShoe from "../assets/running-shoe.svg";
+import styles from "../stylesheets/LandingPage.module.css";
 function LandingPage() {
+    const history = useHistory();
     return (
         <div className={styles.landingPageContainer}>
             <NavBar />
             <div className={styles.landingPageContent}>
                 <div className={styles.landingPageMain}>
                     <div className={styles.landingPageMainContent}>
-                        <h2>
+                        <h2 id={styles.title1}>
                             <span style={{ color: "#fc5200" }}>Strive.</span> Share.
                             Celebrate
                         </h2>
-                        <p>
+                        <p style={{ fontSize: "1.7rem" }}>
                             People on Strava upload everything from weekend hikes to marathons. The only rule is to cheer each other on.
                         </p>
                         <div id={styles.landingPageButtons}>
-                            <button>
+                            <button onClick={_ => history.push("/join")}>
                                 Join for free
                             </button>
-                            <button>
+                            <button onClick={_ => history.push("/login")}>
                                 Log in
                             </button>
                         </div>
                     </div>
-                    <div className={styles.landingPageMainContent}>
-
+                    <div className={styles.landingPageMainImage}>
+                        <img src={heroRunning} alt="Hero running" />
+                        <div id={styles.runningPhone} >
+                            <img src={phone1} alt="Phone with post" />
+                        </div>
                     </div>
                 </div>
                 <div className={styles.landingPageMain}>
-                    <div className={styles.landingPageMainContent}>
-
+                    <img src={banner} alt="Banner" />
+                </div>
+                <div className={styles.landingPageMain}>
+                    <div className={styles.landingPageMainImage}>
+                        <img src={heroBiking} alt="Hero Biking" />
                     </div>
                     <div className={styles.landingPageMainContent}>
-
+                        <h2>
+                            The home of your active life.
+                        </h2>
+                        <ul>
+                            <li className={styles.iconListItem}>
+                                <div className={styles.svgIconContainer}>
+                                    <img src={runningShoe} alt="shoe icon" />
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
