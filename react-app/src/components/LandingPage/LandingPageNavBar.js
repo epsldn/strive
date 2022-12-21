83a68c5cdb63817f4c33eeed980951c4eff83628
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-20 19:44:55 -0600

    navbar skeleton done

--- react-app/src/components/LandingPage/LandingPageNavBar.js
+++ react-app/src/components/LandingPage/LandingPageNavBar.js
@@ -1,8 +1,16 @@
+import styles from "../../stylesheets/LandingPageNavBar.module.css";
+
 function LandingPageNavBar() {
     return (
-        <ul>
-            <li>Hi</li>
-        </ul>
+        <div className={styles.mainContainer}>
+            <nav className={styles.navBarContent}>
+                <p>STRIVE</p>
+                <div id={styles.buttonContainer}>
+                    <button>Log In</button>
+                    <button>Joing for Free</button>
+                </div>
+            </nav>
+        </div>
     );
 }
 
