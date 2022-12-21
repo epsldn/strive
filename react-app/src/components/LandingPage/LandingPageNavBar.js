f2d8c5c152878f03d9db26eb7ec98f215da99be3
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-21 12:00:15 -0600

    Complete styling for auth forms

--- react-app/src/components/LandingPage/LandingPageNavBar.js
+++ react-app/src/components/LandingPage/LandingPageNavBar.js
@@ -1,4 +1,4 @@
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import styles from "../../stylesheets/LandingPageNavBar.module.css";
 
 function LandingPageNavBar() {
@@ -6,7 +6,9 @@ function LandingPageNavBar() {
     return (
         <div className={styles.mainContainer}>
             <nav className={styles.navBarContent}>
-                <p>STRIVE</p>
+                <p style={{ userSelect: "none" }}>
+                    STRIVE
+                </p>
                 <div id={styles.buttonContainer}>
                     <button onClick={_ => history.push("/login")}>Log In</button>
                     <button onClick={_ => history.push("/join")}>Join for Free</button>
