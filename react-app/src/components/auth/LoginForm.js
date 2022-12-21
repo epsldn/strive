a5525717f59880790dbbd7716f0d445a056d4a62
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-21 15:40:28 -0600

    Login form complete w/ error validators

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
@@ -6,19 +6,58 @@ import AuthNavBar from './AuthNavBar';
 import imagePicker from "./util";
 import styles from "../../stylesheets/Auth.module.css";
 
+
+function fieldValidator(email, password, setErrors) {
+  const errors = {};
+  if (!email && !password) {
+    errors.fields = "Both fields are required";
+    setErrors(errors);
+    return errors;
+  }
+
+  if (email.length < 1 || email.length > 255) errors.email = "Plase enter your email";
+  else if (!email.includes("@")) errors.email = "Please enter a valid email";
+  if (password.length < 1 || password.length > 255) errors.password = "Please enter your password";
+  return errors;
+}
+
+function errorFieldAnimation() {
+  const errorField = document.querySelector(".Auth_errorField__2GHOZ");
+  errorField.animate([
+    { padding: "16px", color: "white", height: "fit-content" },
+    { color: "transparent" },
+    { padding: 0, height: 0, color: "transparent" }
+  ],
+    {
+      delay: 1500,
+      fill: "forwards",
+      easing: "ease-out",
+      duration: 300
+    }
+  );
+}
+
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState({});
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
   const [backgroundImage] = useState(imagePicker());
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
   const onLogin = async (e) => {
     e.preventDefault();
+    setSubmitted(true);
+    const errors = fieldValidator(email, password, setErrors);
+    setErrors(errors);
+    if (Object.keys(errors).length > 0) return;
     const data = await dispatch(login(email, password));
     if (data) {
       setErrors(data);
+      setSubmitted(false);
+      errorFieldAnimation();
+      setTimeout(() => setErrors({}), 1800);
     }
   };
 
@@ -38,9 +77,28 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
+  useEffect(() => {
+    if (!submitted) return;
+    const errors = {};
+
+    if (!email && !password) {
+      errors.fields = "Both fields are required";
+      setErrors(errors);
+      return errors;
+    }
+
+    if (email.length < 1 || email.length > 255) errors.email = "Plase enter your email";
+    else if (!email.includes("@")) errors.email = "Please enter a valid email";
+    if (password.length < 1 || password.length > 255) errors.password = "Please enter your password";
+    setErrors(errors);
+    return errors;
+
+  }, [email, password]);
+
   if (user) {
     return <Redirect to='/' />;
   }
+
 
   return (
     <div>
@@ -48,9 +106,15 @@ const LoginForm = () => {
       <div className={styles.mainContent}>
         <img id={styles.backgroundImage} src={backgroundImage} />
         <form className={styles.formContainer} onSubmit={onLogin}>
-          {<div>
-          </div>}
           <h2>Log In</h2>
+          {Object.keys(errors).length > 0 &&
+            <ul className={styles.errorField}>
+              <li>{errors.fields}</li>
+              <li>{errors.email}</li>
+              <li>{errors.password}</li>
+              <li>{errors.serverError}</li>
+            </ul>
+          }
           <div className={styles.formContent}>
             <div>
               <input
