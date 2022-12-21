382cb55a57586006f2743ec106e3c46cb4edfeca
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-20 20:43:28 -0600

    Media query for smartphones top half

--- react-app/src/components/LandingPage/LandingPageNavBar.js
+++ react-app/src/components/LandingPage/LandingPageNavBar.js
@@ -1,16 +1,18 @@
+import { useHistory } from "react-router-dom";
 import styles from "../../stylesheets/LandingPageNavBar.module.css";
 
 function LandingPageNavBar() {
+    const history = useHistory();
     return (
         <div className={styles.mainContainer}>
             <nav className={styles.navBarContent}>
                 <p>STRIVE</p>
                 <div id={styles.buttonContainer}>
-                    <button>Log In</button>
-                    <button>Joing for Free</button>
+                    <button onClick={_ => history.push("/login")}>Log In</button>
+                    <button onClick={_ => history.push("/join")}>Join for Free</button>
                 </div>
-            </nav>
-        </div>
+            </nav >
+        </div >
     );
 }
 
