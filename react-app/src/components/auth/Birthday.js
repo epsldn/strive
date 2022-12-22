7772920e81c73a3b96c77af8f5599c721701a7c6
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-21 20:11:29 -0600

    Working dropdown menu

--- react-app/src/components/auth/Birthday.js
+++ react-app/src/components/auth/Birthday.js
@@ -1,6 +1,6 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../../stylesheets/Birthday.module.css";
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const monthsWith31Days = new Set([1, 3, 5, 7, 8, 10, 12]);
 const monthsWith30Days = new Set([4, 6, 9, 11]);
 
@@ -27,29 +27,54 @@ const daysLengthCalculator = (month) => {
 };
 
 function monthFinder(letters) {
+    if (!letters) return MONTHS;
+    letters = letters.slice(0, 1).toUpperCase() + letters.slice(1).toLowerCase();
     const months = [];
-    for (const month in months) {
-        if (month.startsWith(letters)) month.push(month);
+    for (const month of MONTHS) {
+        console.log(month, letters);
+        if (month.startsWith(letters)) months.push(month);
     }
 
-    return months;
-}
+    if (months.length > 0) {
+        return months;
+    }
 
-function updateMonth(e, setMonth) {
-
+    return ["No results."];
 }
 
 function Birthday(props) {
     const [month, setMonth] = useState("");
+    const [months, setMonths] = useState([]);
     const [showMonths, setShowMonths] = useState(false);
     const [day, setDay] = useState("");
     const [showDays, setShowDays] = useState(false);
     const [year, setYear] = useState("");
     const [showYear, setShowYear] = useState(false);
     const { setShowModal } = props;
+    const container1 = useRef(null);
+    const container2 = useRef(null);
+    const container3 = useRef(null);
     const ref1 = useRef(null);
     const ref2 = useRef(null);
     const ref3 = useRef(null);
+
+    useEffect(() => {
+        const months = monthFinder(month);
+        setMonths(monthFinder(month));
+    }, [month]);
+
+    useEffect(() => {
+        if (!showMonths) return;
+        console.log("HELLO");
+        function onClick(e) {
+            if (container1.current && container1.current.contains(e.target) === false) {
+                setShowMonths(false);
+            }
+        }
+        document.addEventListener("click", onClick);
+        return () => document.removeEventListener("click", onClick);
+    }, [showMonths]);
+
     return (
         <div className={styles.container}>
             <i id={styles.x} className="fa-solid fa-x" onClick={() => setShowModal(false)} />
@@ -58,30 +83,37 @@ function Birthday(props) {
                 <form>
                     <h3>Birthday</h3>
                     <div className={styles.formContent}>
-                        <div className={styles.inputContainer} onClick={() => {
-                            setShowMonths(true);
-                            ref1.current.focus();
-                        }
-                        }>
+                        <div className={styles.inputContainer}
+                            onClick={() => {
+                                setShowMonths(true);
+                                ref1.current.focus();
+                            }} ref={container1}>
                             <input
                                 type="text"
                                 ref={ref1}
                                 placeholder={"MONTH"}
                                 value={month}
                                 onChange={(event) => setMonth(event.target.value)}
-                                onBlur={() => setShowMonths(false)}
                             />
                             <i className="fa-solid fa-caret-down" />
                             {showMonths &&
-                                <div className={styles.dropdown}>
-                                    "HELLO"<br />
-                                    "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO"
-                                </div>
+                                <ul className={styles.dropdown}>
+                                    {
+                                        months.map(month => {
+                                            if (month === "No results.") {
+                                                return <li className={styles.dropdownItem} style={{ backgroundColor: "white" }} key={month}>{month}</li>;
+                                            }
+                                            return (
+                                                <li className={styles.dropdownItem} key={month} style={{ cursor: "pointer" }} onClick={(event) => {
+                                                    event.preventDefault();
+                                                    event.stopPropagation();
+                                                    setMonth(month);
+                                                    setShowMonths(false);
+                                                }}>{month}</li>
+                                            );
+                                        })
+                                    }
+                                </ul>
                             }
                         </div>
                         <div className={styles.inputContainer} onClick={() => {
@@ -99,15 +131,9 @@ function Birthday(props) {
                             />
                             <i className="fa-solid fa-caret-down" />
                             {showDays &&
-                                <div className={styles.dropdown}>
-                                    "HELLO"<br />
-                                    "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO"
-                                </div>}
+                                <ul className={styles.dropdown}>
+
+                                </ul>}
                         </div>
                         <div className={styles.inputContainer} onClick={() => {
                             setShowYear(true);
@@ -125,24 +151,18 @@ function Birthday(props) {
                             />
                             <i className="fa-solid fa-caret-down" />
                             {showYear &&
-                                <div className={styles.dropdown}>
-                                    "HELLO"<br />
-                                    "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO" "HELLO"<br />
-                                    "HELLO"
-                                </div>}
+                                <ul className={styles.dropdown}>
+
+                                </ul>}
                         </div>
                     </div>
                 </form>
                 <p>By signing up for Strive, you don't have to agree to anything. Strive is not a real company</p>
-            </div>
+            </div >
             <div className={styles.agreeButtonContainer}>
                 <button>Agree and Sign Up</button>
             </div>
-        </div>
+        </div >
     );
 }
 
