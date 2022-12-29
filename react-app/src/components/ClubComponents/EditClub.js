60f7b4f0126d216c12e424050328312d5b3c96bc
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-28 22:22:38 -0600

    working city search

--- react-app/src/components/ClubComponents/EditClub.js
+++ react-app/src/components/ClubComponents/EditClub.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Redirect, useHistory, useParams } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { Redirect, useHistory, useLocation, useParams } from "react-router-dom";
 import MainNavBar from "../MainNavBar";
 import styles from "../../stylesheets/ClubForm.module.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,12 +15,25 @@ function EditClub() {
     const [clubType, setClubType] = useState("club");
     const [description, setDescription] = useState("");
     const [loaded, setLoaded] = useState(false);
+    const [coordinates, setCoordinates] = useState("");
+    const [selected, setSelected] = useState(0);
+    const [showCities, setShowCities] = useState(true);
+    const [cities, setCities] = useState([]);
+
     const { clubId } = useParams();
     const clubs = useSelector(state => state.clubs);
     const [club, setClub] = useState(null);
+
+
+    const path = useLocation().pathname;
     const history = useHistory();
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user);
+
+    const locationRef = useRef(null);
+    const locationContainer = useRef(null);
+    const cityRef = useRef(null);
+
     document.title = `${club?.clubName || "Edit Club"} | Strive Club`;
 
     useEffect(() => {
@@ -73,9 +86,76 @@ function EditClub() {
         dispatch(deleteClub(clubId));
     }
 
+
+    function handleKeyDown(event) {
+        const key = event.code;
+        console.log(event, key);
+
+
+        if (key === "ArrowUp" && selected > 0) {
+            event.preventDefault();
+            setSelected(selected => selected - 1);
+        }
+
+        if (key === "ArrowDown" && selected < 4) {
+            event.preventDefault();
+            setSelected(selected => selected + 1);
+        }
+
+        if (key === "Enter" && cityRef.current) {
+            event.preventDefault();
+            setCities([cityRef.current.textContent]);
+            setLocation(cityRef.current.textContent);
+            setShowCities(false);
+        }
+
+        if (key === "Tab" && cityRef.current) {
+            setShowCities(false);
+        }
+    }
+
+    function handleCancel(event) {
+        event.preventDefault();
+        history.goBack();
+    }
+
+    useEffect(() => {
+        fetch("/api/maps/city-search", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ coordinates, search: location })
+        })
+            .then(response => response.json())
+            .then(data => {
+                setCities(data);
+            });
+
+        if (locationContainer.current && cities.length > 1) {
+            setShowCities(true);
+        }
+
+        function onClick(e) {
+            if (locationContainer.current && locationContainer.current.contains(e.target) === false) {
+                setShowCities(false);
+            }
+        }
+
+        document.addEventListener("click", onClick);
+        return () => document.removeEventListener("click", onClick);
+    }, [location]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => setCoordinates(position.coords.latitude + "," + position.coords.longitude),
+            () => setCoordinates(null),
+            { enableHighAccurary: true, timeout: 3000 });
+    }, [path]);
+
     if (loaded && clubId in user.owned_clubs === false) {
         return <Redirect to="/" />;
     }
+
+
 
     return (
         <div className={styles.mainWrapper}>
@@ -94,13 +174,46 @@ function EditClub() {
                             />
                             <label className={styles.errors}>{errors.clubName}</label>
                         </div>
-                        <div>
+                        <div ref={locationContainer} onKeyDown={handleKeyDown}>
                             <label>Location *</label>
                             <input
                                 type="text"
                                 onChange={(event) => setLocation(event.target.value)}
                                 value={location}
+                                id={styles.location}
+                                ref={locationRef}
+                                onFocus={() => setShowCities(true)}
+                                autoComplete="off"
+                                onBlur={() => {
+                                    if (location !== cities[0]) {
+                                        setLocation("");
+                                    }
+                                }}
+
                             />
+                            {showCities && cities.length > 0 &&
+                                <ul className={styles.cities}>
+                                    {cities.map((city, index) => (
+                                        <li key={city}
+                                            style={
+                                                selected === index ?
+                                                    { backgroundColor: "lightgray" } :
+                                                    {}
+                                            }
+                                            ref={
+                                                selected === index ?
+                                                    cityRef :
+                                                    undefined
+                                            }
+                                            onClick={() => {
+                                                setLocation(city);
+                                                setShowCities(false);
+                                            }}
+                                        >{city}</li>
+                                    )
+                                    )}
+                                </ul>
+                            }
                             <label className={styles.errors}>{errors.location}</label>
                         </div>
                         <div>
