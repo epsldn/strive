8bfce4ec4db014013c39fbd0fe6b126a44b978d3
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-01-02 15:12:38 -0600

    Activity form v1

--- app/forms/activity_form.py
+++ app/forms/activity_form.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, ValidationError, NumberRange, Length
+from wtforms import StringField, DateField, IntegerField, TextAreaField, FloatField
+from wtforms.validators import DataRequired, ValidationError, NumberRange, Length, Optional
 
 
 class ActivityForm(FlaskForm):
-    pass
+    user_id = IntegerField("User id", [DataRequired("Owner Id required")])
+    distance = FloatField("Distance", [Optional(), NumberRange(min=0)])
+    hours = FloatField("Hours", [Optional(), NumberRange(min=0)])
+    minutes = FloatField("Minutes", [Optional(), NumberRange(min=0, max=60)])
+    seconds = FloatField("Seconds", [Optional(), NumberRange(min=0, max=60)])
+    elevation = FloatField("Elevation", [Optional(), NumberRange(max=29029)])
+    sport = StringField("Sport", [DataRequired(
+        "Please enter a sport"), Length(min=1, max=100)])
+    date = DateField("Date", [Optional()])
+    time = DateField("Time", [Optional()])
+    title = StringField("Title", [Optional()])
+    description = StringField("Description", [Optional()])
+    private_notes = StringField("Private Notes", [Optional()])
