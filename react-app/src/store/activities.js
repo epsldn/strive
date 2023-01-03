f152bb19e5fe0f0f1ecfd6ce50757bd5c51ea482
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-01-02 22:06:35 -0600

    load activities in to the store

--- react-app/src/store/activities.js
+++ react-app/src/store/activities.js
@@ -1,14 +1,33 @@
 const GET_ACTIVITIES = "activities/get";
 const ADD_ACTIVITY = "activites/add";
-const UPDATE_ACTIVITY = "activites/update";
+const UPDATE_ACTIVITY = "activities/update";
 const DELETE_ACTIVITY = "activities/delete";
 
 
+const setActivitiesStore = (activities) => {
+    return ({
+        type: GET_ACTIVITIES,
+        activities
+    }
+    );
+};
 
+export const fetchActivities = () => async dispatch => {
+    const response = await fetch("/api/activities/");
+    const data = await response.json();
+    if (response.ok) {
+        dispatch(setActivitiesStore(data));
+    } else {
+        return data;
+    }
+};
 
 const initialState = { activites: [] };
 export default function reducer(state = initialState, action) {
     switch (action.type) {
+        case GET_ACTIVITIES: {
+            return action.activities;
+        }
         default:
             return state;
     }
