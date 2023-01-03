0da0377832900f97dc0c92dbc4944115b4dd2511
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-01-02 23:45:30 -0600

    add buttons for edit activity and delete activity

--- react-app/src/components/ActivityComponents/ActivityShowCase.js
+++ react-app/src/components/ActivityComponents/ActivityShowCase.js
@@ -5,15 +5,26 @@ import MainNavBar from "../MainNavBar";
 
 function ActivityShowCase() {
     const { activityId } = useParams();
+    const user = useSelector(state => state.session.user);
     const activity = useSelector(state => state.activites[activityId]);
 
     return (
         <div className={styles.pageOuterContainer}>
             <MainNavBar />
             <div className={styles.activitiesContainer}>
-                "HELLO"
+                {
+                    user?.id === activity?.user_id &&
+                    <div className={styles.iconsContainer}>
+                        <div className={styles.iconContainer}>
+                            <i className="fa-solid fa-pencil" />
+                        </div>
+                        <div className={styles.iconContainer}>
+                            <i className="fa-regular fa-trash-can" />
+                        </div>
+                    </div>
+                }
             </div>
-        </div>
+        </div >
     );
 }
 
