e85aaf20e9003e191db288d5b7c60f15b06d1a0b
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-01-03 23:38:15 -0600

    proper data being displayed

--- app/forms/activity_form.py
+++ app/forms/activity_form.py
@@ -6,9 +6,9 @@ from wtforms.validators import DataRequired, ValidationError, NumberRange, Lengt
 class ActivityForm(FlaskForm):
     user_id = IntegerField("User id", [DataRequired("User Id required")])
     distance = FloatField("Distance", [Optional(), NumberRange(min=0)])
-    hours = FloatField("Hours", [Optional(), NumberRange(min=0)])
-    minutes = FloatField("Minutes", [Optional(), NumberRange(min=0, max=60)])
-    seconds = FloatField("Seconds", [Optional(), NumberRange(min=0, max=60)])
+    hours = IntegerField("Hours", [Optional(), NumberRange(min=0)])
+    minutes = IntegerField("Minutes", [Optional(), NumberRange(min=0, max=60)])
+    seconds = IntegerField("Seconds", [Optional(), NumberRange(min=0, max=60)])
     elevation = FloatField("Elevation", [Optional(), NumberRange(max=29029)])
     extertion = IntegerField(
         "Extertion level", [Optional(), NumberRange(min=1, max=10)])
--- react-app/src/components/ActivityComponents/ActivityShowCase.js
+++ react-app/src/components/ActivityComponents/ActivityShowCase.js
@@ -46,9 +46,21 @@ function ActivityShowCase() {
     }
 
     let date;
+    let pace;
 
     if (activity) {
         date = new Date(activity.date + " " + activity.time);
+
+        const totalSeconds = (Number(activity.hours) * 3600) + Number(activity.minutes * 60) + Number(activity.seconds);
+        const dividedSeconds = totalSeconds / Number(activity.distance);
+        let hours = Math.floor(dividedSeconds / 3600);
+        let minutes = Math.floor((dividedSeconds % 3600) / 60);
+        let seconds = Math.floor(dividedSeconds) % 60;
+        if (hours < 10) hours = "0" + hours;
+        if (minutes < 10) minutes = "0" + minutes;
+        if (seconds < 10) seconds = "0" + seconds;
+
+        pace = Number(hours) !== 0 ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`;
     }
 
     return (
@@ -95,9 +107,21 @@ function ActivityShowCase() {
                             </div>
                         </div>
                         <div className={styles.activityBodyRight}>
-                            <div id={styles.activityTime}>
-                                {`${activity?.hours}:${formatTime(activity?.minutes)}:${formatTime(activity?.seconds)}`}
+                            {Boolean(activity?.distance) && <div className={styles.activityBodyRightContent}>
+                                <p>{Number(activity.distance).toFixed(2)} mi</p>
+                                <p>Distance</p>
+                            </div>}
+                            <div className={styles.activityBodyRightContent}>
+                                <p>{`${activity?.hours}:${formatTime(activity?.minutes)}:${formatTime(activity?.seconds)}`}</p>
                                 <p>Duration</p>
+                            </div>
+                            {Boolean(activity?.distance) && <div className={styles.activityBodyRightContent}>
+                                <p>{pace}/mi</p>
+                                <p>Pace</p>
+                            </div>}
+                            <div className={styles.activityBodyRightContent}>
+                                <p>{activity?.elevation} ft</p>
+                                <p>Elevation</p>
                             </div>
                         </div>
                     </div>
