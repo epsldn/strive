e6fdf5183e9a46c4df596384b3021f57204ac13e
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2023-02-05 23:16:17 -0600

    Add follow requsts table

--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,12 @@ followers = db.Table("followers",
                                db.ForeignKey("users.id"))
                      )
 
+follow_requests = db.Table("follow_requets",
+                            db.Column("requested_id", db.Integer,
+                                        db.ForeignKey("users.id")),
+                            db.Column("requestor_id", db.Integer,
+                                        db.ForeignKey("users.id"))
+                                        )
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -44,6 +50,15 @@ class User(db.Model, UserMixin):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref("followers", lazy="dynamic"),
+        lazy="dynamic"
+    )
+
+    follow_requests = db.relationship(
+        "User",
+        secondary=follow_requests,
+        primaryjoin=(follow_requests.c.requested_id == id),
+        secondaryjoin=(follow_requests.c.requestor_id == id),
+        backref=db.backref("follow_requests", lazy="dynamic"),
         lazy="dynamic"
     )
 
